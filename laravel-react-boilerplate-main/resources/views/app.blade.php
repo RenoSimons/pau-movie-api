ddd82382562dd2f79fd137af0d1f3c6558963006
reno simons <simons.reno@skynet.be>
2021-10-12 14:43:52 +0200

    Initialized Laravel/react/inertia stack

---
+++ laravel-react-boilerplate-main/resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Movie-app</title>
+
+        <!-- Styles -->
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+
+        <!-- Scripts -->
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    </head>
+    <body>
+        @inertia
+    </body>
+</html>
